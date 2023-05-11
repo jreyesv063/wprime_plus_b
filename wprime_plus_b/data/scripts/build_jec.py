3b7466579cb78d1da7e81b7f66473fcf08d42745
deoache <daniel.ocampoh@udea.edu.co>
2023-05-11 23:10:58 +0000

    fix JEC/JER correction names

--- wprime_plus_b/data/scripts/build_jec.py
+++ wprime_plus_b/data/scripts/build_jec.py
@@ -23,23 +23,29 @@ jec_name_map = {
 def jet_factory_factory(files):
     data_path = "/home/cms-jovyan/wprime_plus_b/wprime_plus_b/data/"
     ext = extractor()
-    ext.add_weight_sets([f"* * {data_path}/{file}" for file in files])
+    ext.add_weight_sets([
+        f"* * {data_path}/{file}" for file in files
+    ])
     ext.finalize()
     jec_stack = JECStack(ext.make_evaluator())
     return CorrectedJetsFactory(jec_name_map, jec_stack)
 
-
+# https://twiki.cern.ch/twiki/bin/viewauth/CMS/JECDataMC#Recommended_for_MC
 jet_factory = {
     "2017mc": jet_factory_factory(
         files=[
-            # https://github.com/cms-jet/JRDatabase/raw/master/textFiles/Fall17_V3b_MC/Fall17_V3b_MC_PtResolution_AK4PFchs.txt
-            "Fall17_V3b_MC_PtResolution_AK4PFchs.jr.txt.gz",
-            # https://github.com/cms-jet/JRDatabase/raw/master/textFiles/Fall17_V3b_MC/Fall17_V3b_MC_SF_AK4PFchs.txt
-            "Fall17_V3b_MC_SF_AK4PFchs.jersf.txt.gz",
+            # JEC: https://github.com/cms-jet/JECDatabase/tree/master/textFiles
+            "Summer19UL17_V5_MC_L1FastJet_AK4PFchs.jec.txt",
+            "Summer19UL17_V5_MC_L2L3Residual_AK4PFchs.jec.txt",
+            "Summer19UL17_V5_MC_L2Relative_AK4PFchs.jec.txt",
+            "Summer19UL17_V5_MC_L3Absolute_AK4PFchs.jec.txt",
+            "RegroupedV2_Summer19UL17_V5_MC_UncertaintySources_AK4PFchs.junc.txt",
+            # JER: https://github.com/cms-jet/JRDatabase/tree/master/tarballs
+            "Summer19UL17_JRV2_MC_PtResolution_AK4PFchs.jr.txt",
+            "Summer19UL17_JRV2_MC_SF_AK4PFchs.jersf.txt",
         ]
     )
 }
-
 met_factory = CorrectedMETFactory(jec_name_map)
 
 
