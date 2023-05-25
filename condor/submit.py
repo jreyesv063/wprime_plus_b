0c315b1f5c924b05eb79891161e760a56234931c
deoache <daniel.ocampoh@udea.edu.co>
2023-05-25 03:29:44 +0000

    update path definition

--- condor/submit.py
+++ condor/submit.py
@@ -2,52 +2,52 @@ import os
 import json
 import argparse
 import subprocess
-import importlib.resources
 from pathlib import Path
 
+
 def main(args):
-    local_dir = Path.cwd()
-    main_dir = local_dir.parent
-    
-    # create logs directory
-    log_dir = Path(str(local_dir) + "/logs")
+    main_dir = Path.cwd()
+    condor_dir = Path(f"{main_dir}/condor")
+
+    # create logs and output directories
+    log_dir = Path(str(condor_dir) + "/logs")
     if not log_dir.exists():
         log_dir.mkdir()
-    
-    # create output directory
-    username = os.environ["USER"]
-    eos = Path(f"/eos/user/{username[0]}/{username}")
-    out_dir = Path(f"{local_dir}/out/")#Path(str(eos) + args.tag)
+        
+    # username = os.environ["USER"]
+    # eos = Path(f"/eos/user/{username[0]}/{username}")
+    # out_dir = Path(str(eos) + args.tag)
+    out_dir = Path(f"{condor_dir}/out/")
     if not out_dir.exists():
         out_dir.mkdir(parents=True)
-    
+        
     # load fileset
     with open(f"{main_dir}/wprime_plus_b/data/simplified_samples.json", "r") as handle:
         simplified_samples = json.load(handle)[args.year]
-            
+        
+    # submit condor jobs
     for sample in list(simplified_samples.values()):
         print(f"submitting {sample}")
-        sample_dir = Path(f"{str(out_dir)}/sample")
-        if not sample_dir.exists():
-            sample_dir.mkdir(parents=True)
-        
+        # create sample directory
+        sample_out_dir = Path(f"{out_dir}/{sample}")
+        if not sample_out_dir.exists():
+            sample_out_dir.mkdir(parents=True)
         # make condor file
-        condor_templ_file = open(f"{local_dir}/submit.sub")
-        local_condor = f"{local_dir}/{sample}.sub"
+        condor_template_file = open(f"{condor_dir}/submit.sub")
+        local_condor = f"{condor_dir}/{sample}.sub"
         condor_file = open(local_condor, "w")
-        for line in condor_templ_file:
-            line = line.replace("DIRECTORY", str(local_dir))
+        for line in condor_template_file:
+            line = line.replace("DIRECTORY", str(condor_dir))
             line = line.replace("PREFIX", sample)
             condor_file.write(line)
         condor_file.close()
-        condor_templ_file.close()
-        
+        condor_template_file.close()
+
         # make executable file
-        sh_templ_file = open(f"{local_dir}/submit.sh")
-        local_sh = f"{local_dir}/{sample}.sh"
-        eosoutput_dir = args.output_location#Path(f"{str(out_dir)}/{sample}")
+        sh_template_file = open(f"{condor_dir}/submit.sh")
+        local_sh = f"{condor_dir}/{sample}.sh"
         sh_file = open(local_sh, "w")
-        for line in sh_templ_file:
+        for line in sh_template_file:
             line = line.replace("MAINDIRECTORY", str(main_dir))
             line = line.replace("PROCESSOR", args.processor)
             line = line.replace("EXECUTOR", args.executor)
@@ -61,19 +61,12 @@ def main(args):
             line = line.replace("TAG", args.tag)
             sh_file.write(line)
         sh_file.close()
-        sh_templ_file.close()
-        
+        sh_template_file.close()
+
         # submit jobs
-        with open(local_sh, "r") as file:
-            bash_script = file.read()
-            subprocess.run(['bash', '-c', bash_script], check=True)
-        
-        # remove .sub and .sh files
-        os.remove(local_condor)
-        os.remove(local_sh)
-    
-    
-    
+        subprocess.run(["condor_submit", local_condor])
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -123,7 +116,7 @@ if __name__ == "__main__":
         "--redirector",
         dest="redirector",
         type=str,
-        default="xcache",
+        default="cmsxrootd.fnal.gov",
         help="redirector to acces data {xcache to use at coffea-casa}",
     )
     parser.add_argument(
--- condor/submit.sh
+++ condor/submit.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+export XRD_NETWORKSTACK=IPv4
 export X509_USER_PROXY=$HOME/tmp/x509up
 cd MAINDIRECTORY
 
 python3 run_condor.py --processor PROCESSOR --executor EXECUTOR --workers WORKERS --sample SAMPLE --channel CHANNEL --nfiles NFILES --year YEAR --redirector REDIRECTOR --output_location OUTPUTLOCATION --tag TAG
+
+
+
