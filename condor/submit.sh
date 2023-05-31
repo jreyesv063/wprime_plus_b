836067feb857acdc9b673bbe607e70e03ec62b17
deoache <daniel.ocampoh@udea.edu.co>
2023-05-31 04:25:46 +0000

    update to use run.py

--- condor/submit.sh
+++ condor/submit.sh
@@ -4,6 +4,6 @@ export XRD_NETWORKSTACK=IPv4
 export X509_USER_PROXY=$HOME/tmp/x509up
 cd MAINDIRECTORY
 
-python3 run_condor.py --processor PROCESSOR --executor EXECUTOR --workers WORKERS --channel CHANNEL --nfiles NFILES --year YEAR --redirector REDIRECTOR --output_location OUTPUTLOCATION --tag TAG --fileset FILESET
+python3 run.py --processor PROCESSOR --executor EXECUTOR --workers WORKERS --channel CHANNEL --nfiles NFILES --year YEAR --redirector REDIRECTOR --output_location OUTPUTLOCATION --tag TAG --fileset FILESET
 
 xrdcp -r condor/out/ EOSDIR
